a1b22a857401d65cbada445cc2413d9a2dbf8cf5
seaneff <steph.eaneff@gmail.com>
2023-10-20 13:17:36 -0700

    initial commit

---
+++ 2023/week42_taylorswift/analysis.R
@@ -0,0 +1,91 @@
+#######################################################################
+### Background ########################################################
+#######################################################################
+
+## Definitions of key data points, see https://taylor.wjakethompson.com/reference/taylor_songs
+## Dancibility: How suitable a track is for dancing. 0.0 = least danceable, 1.0 = most danceable (based on spotify API)
+## Valence: Musical positiveness conveyed by the track. 0.0 = low valence (e.g., sad, depressed, angry), 1.0 = high valence (e.g., happy, cheerful, euphoric).
+
+#######################################################################
+### Load required libraries and fonts #################################
+#######################################################################
+
+## if you don't already have readxl downloaded, start by running:
+## install.packages("tidytuesdayR"), etc
+library(tidytuesdayR) ## to read in data for tidytuesday
+library(tidyverse) ## to format/restructure/plot data
+library(ggrepel) ## for help positioning scatterplot labels
+library(taylor) ## for custom taylor swift palettes
+library(showtext) ## for custom fonts
+library(ggtext) ## for combining bold and not bold fonts on axis label
+library(glue) ## for combining bold and not bold fonts on axis label
+
+## to review current list of options, if needed
+#font_families_google()
+
+font_add_google("Barlow")
+
+#######################################################################
+### Load data #########################################################
+#######################################################################
+
+tuesdata <- tidytuesdayR::tt_load(2023, week = 42)
+
+taylor_album_songs <- tuesdata$taylor_album_songs
+taylor_all_songs <- tuesdata$taylor_all_songs
+taylor_albums <- tuesdata$taylor_albums
+
+#######################################################################
+### Generate Figure ###################################################
+#######################################################################
+
+taylor_album_songs$label <- 
+  ifelse(taylor_album_songs$track_name %in% 
+           c("Shake It Off", "Stay Stay Stay", "Paper Rings", "Hey Stephen (Taylor's Version)",
+             "You Need to Calm Down", "How You Get the Girl", "Gorgeous", "I Think He Knows",
+             "Cornelia Street", "Clean", "Vigilante Shit", "Delicate", "Maroon", "closure",
+             "This Love (Taylor's Version)", "epiphany", "The Archer", "the lakes", "Bigger Than The Whole Sky",
+             "willow", "Better Than Revenge", "Mean", "Sweet Nothing", "How You Get The Girl"),
+         taylor_album_songs$track_name, "")
+
+valence_vs_danceability <- taylor_album_songs %>% 
+  filter(complete.cases(danceability)) %>%
+  filter(complete.cases(valence)) %>%
+  ggplot(aes(x = danceability, 
+             y = valence, 
+             label = label)) +
+  geom_point(aes(alpha = valence,
+                 color = danceability)) +
+  geom_point(shape = 21, color = "grey40", stroke = 0.2) +
+  labs(x = "", 
+       y = "",
+       title = "Which Taylor Swift songs are most danceable?",
+       subtitle = "Exploring happier vs. sadder TSwift songs based on their danceability") +
+  theme_minimal() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        legend.position = "none",
+        text = element_text(colour = "grey20",
+                            family = "Barlow"),
+        strip.text = element_text(color = "grey1"),
+        plot.title = element_text(colour = "grey1",
+                                  size = rel(1.5), 
+                                  face = "bold"),
+        axis.text.x = element_markdown(),
+        axis.text.y = element_markdown()) +
+  scale_x_continuous(breaks = c(0.4, .8),
+                     labels = c("**Less danceable**<br>(per Spotify)", 
+                                "**More danceable**<br>(per Spotify)")) +
+  scale_y_continuous(breaks = c(0.2, 0.9),
+                     labels = c("**Lower valence**<br>(e.g., sad, depressed, angry)", 
+                                "**Higher valence**<br>(e.g., happy, cheerful, euphoric)")) +
+  scale_alpha(range = c(0.2, 1)) +
+  scale_color_gradient(low = "#0000FF", high = "#EA5F94") +
+  geom_text_repel(size = 3, min.segment.length = 0.1) 
+valence_vs_danceability
+
+ggsave(plot = valence_vs_danceability,
+       filename = "valence_vs_danceability.png", 
+       dpi = 350, height = 4, width = 8, units = "in",
+       bg = 'white')
+
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# tidytuesday
-Assorted data visualizations associated with tidy-tuesday project
+![Logo for the TidyTuesday project, represented by the word TidyTuesday over a messy splash of black paint](static/tt_logo.png)
+
+## About TidyTuesday
+
+`TidyTuesday` is a weekly social data project hosted and organized by the R4DS Data Science community. The overarching goal of TidyTuesday is to make learning to work with data easier by providing real-world datasets.
+
+New data are posted every Monday morning. You can use these data to create a visualization, a model, a shiny app, or some other piece of data-science-related output, using R or another programming language. For more information on TidyTuesday, please see the [rfordatascience/tidytuesday github repository](https://github.com/rfordatascience/tidytuesday).
+
+
+## Steph's Data Viz
+
+I'm using this as an opportunity to learn and practice some new things in R, especially data visualization. I'm not going to update every week, but I'll post code for the challenges I do complete. See below for some examples of figures I've created based on this challenge (all code is also contained in this github repo).
+
+### Danceability of Taylor Swift Songs
+
+![Figure illustrating the relationship between valence and danceability for Taylor Swift songs, highlighting some of the most and least danceable songs, as well as some of the happiest and saddest songs](2023/week42_taylorswift/valence_vs_danceability.png)
